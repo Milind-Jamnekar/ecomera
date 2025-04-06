998e81d43312a014bfd4da652c3637686b0f7ca4
Milind Jamnekar <milindjamnekar502@gmail.com>
2025-04-06 17:12:51 +0530

    feat: update README and package.json for Ecomera project
    - Updated README.md to reflect the new project name and description.
    - Added essential dependencies for database connection and validation in package.json.
    - Updated pnpm-lock.yaml to include new dependencies.
    - Configured TypeORM and environment variable validation in app.module.ts.
    - Modified main.ts to retrieve the port from environment variables using ConfigService.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        DB: joi.string(),
+        DB_HOST: joi.string().required(),
+        DB_PORT: joi.number().default(5432),
+        DB_USER: joi.string().required(),
+        DB_PASSWORD: joi.string().required(),
+        DB_NAME: joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
